3b224e03934ff1cd3edea720593166d6ac7c787a
Carl Kakisis <Messinias1@gmail.com>
2019-05-18 13:22:50 -0400

    created new post and get axios routes

--- public/script.js
+++ public/script.js
@@ -4,5 +4,32 @@ function getUserInfo() {
    axios.get("/showprofile/" + input)
     .then(response => {
         console.log("axios", response.data)
+        document.getElementById("results").innerHTML = response.data + input;
     })
+}
+
+function handleSubmit() {
+    const userName = document.getElementById("user-name").value
+    const userId = document.getElementById("user-id").value
+    const message = document.getElementById("message").value
+    console.log(userName, userId, message)
+
+    const payload = {
+        userName,
+        userId,
+        message
+    }
+
+    axios.post("/api", payload)
+        .then(response => {
+            console.log(response.data)
+        })
+}
+
+function getAllUsers() {
+
+    axios.get("getallusers")
+        .then(response => {
+            document.getElementById('result').innerHTML = JSON.stringify(response.data)
+        })
 }
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static("public"))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello Carl!'))
 
+var data = [];
+
 // query object
-app.get('/api', (req, res) => {
-    const userName = req.query.username;
-    const userId = req.query.id;
-    const message = req.query.message;
+app.post('/api', (req, res) => {
+    const userName = req.body.userName;
+    const userId = req.body.userId;
+    const message = req.body.message;
+
+    const temp = {
+        userName,
+        userId,
+        message
+    }
+
+    data.push(temp)
+    console.log(data)
+
     // console.log(userName)
     // console.log(userId)
     // console.log(message)
     const reply = `${userName} with id of ${userId} is saying ${message}!`
     res.send(reply)
+})
+
+app.get("/getallusers", function (req, res) {
+    res.send(data)
 })
 
 // parameter object
